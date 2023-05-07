77926aedd09cd20b4ba2677ef18ed4c61d4e834c
Harsh Pailkar <85762743+harshpailkar@users.noreply.github.com>
2023-05-07 18:54:01 +0530

    Update Channels.js

--- src/components/Channels.js
+++ src/components/Channels.js
@@ -1,58 +1,74 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
+
 const Channels = ({ provider, account, soulfile }) => {
+    const [image, setImage] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isUploaded, setIsUploaded] = useState(false);
+    const [error, setError] = useState(null);
 
-  const [image, setImage] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isUploaded, setIsUploaded] = useState(false);
+    const handleImageChange = (e) => {
+        setImage(e.target.files[0]);
+    };
 
- 
-async function handleSubmit(e) {
-    e.preventDefault();
-    setIsLoading(true);
-    const signer = await provider.getSigner()
-    const transaction = await soulfile.connect(signer).safeMint(account, "QmcKWqayEfVL6KujBRLZpgPs9vDemPFr3UhvB86NX9Ni3G")
-    await transaction.wait()
+    async function handleSubmit(e) {
+        if (!image) {
+            setError('Please select a file');
+        } else {
+            console.log(image);
+            setError(null);
+            e.preventDefault();
+            setIsLoading(true);
+            const signer = await provider.getSigner();
+            const transaction = await soulfile
+                .connect(signer)
+                .safeMint(
+                    account,
+                    "QmcKWqayEfVL6KujBRLZpgPs9vDemPFr3UhvB86NX9Ni3G"
+                );
+            await transaction.wait();
 
-    setTimeout(() => {
-      // handle the form submission here
-      console.log(image);
-      // simulate a delay of 2 seconds to hide the loader and show the popup
-      setIsLoading(false);
-      setIsUploaded(true);
-      setTimeout(() => {
-        setIsUploaded(false);
-      }, 2000);
-    }, 2000);
-  };
-  
-  const handleImageChange = (e) => {
-    setImage(e.target.files[0]);
-  };
+            setTimeout(() => {
+                // handle the form submission here
+                console.log(image);
+                // simulate a delay of 2 seconds to hide the loader and show the popup
+                setIsLoading(false);
+                setIsUploaded(true);
+                setTimeout(() => {
+                    setIsUploaded(false);
+                }, 2000);
+            }, 2000);
+        }
+    }
 
-  return (
-    <div className="fileupload">
-      <h3>Upload a file to convert it to a Soul Token!</h3>
-      <div className="spacer"></div>
+    return (
+        <div className="fileupload">
+            <h2>Convert your images to</h2>
+            <h1 className="gradient-text">SoulBound NFTs</h1>
+            <div className="spacer"></div>
 
-      <form onSubmit={handleSubmit}>
-      <label htmlFor="image-upload">Choose an image:</label>
-      <input
-        id="image-upload"
-        type="file"
-        accept="image/*"
-        onChange={handleImageChange}
-      />
-      <button type="submit" className='btn__upload'>Upload</button>
-      <div className="spacer"></div>
-      {isLoading && <p>Loading...</p>}
-      {isUploaded && (
-        <div>
-          <p>File uploaded successfully!</p>
+            <form onSubmit={handleSubmit}>
+                {/* <label htmlFor="image-upload">Choose an image:</label> */}
+                <input
+                    id="image-upload"
+                    type="file"
+                    accept="image/*"
+                    className="custom-file-input"
+                    onChange={handleImageChange}
+                />
+                <button type="submit" className="btn__upload" disabled={!image}>
+                    Upload
+                </button>
+                {error && <div style={{ color: 'red' }}>{error}</div>}
+                <div className="spacer"></div>
+                {isLoading && <p>Loading...</p>}
+                {isUploaded && (
+                    <div>
+                        <p>File uploaded successfully!</p>
+                    </div>
+                )}
+            </form>
         </div>
-      )}
-    </form>
-    </div>
-  );
-}
+    );
+};
 
 export default Channels;
